165d610dee202b7cb61493cad03773d23bf9e3b1
IntersticeGames <intersticegamesdev@gmail.com>
2018-10-01 11:23:34 -0500

    Activity 1

---
+++ Scripts/test.js
@@ -0,0 +1,10 @@
+function initialize(){
+	// Called when simulation starts
+	// Add initialization code
+
+}
+function On_Clicked(){
+  eon.FindNode("Cube").GetFieldByName("Hidden").value = false;
+  eon.FindNode("Cube1").GetFieldByName("Hidden").value = false;
+  eon.FindNode("Cube2").GetFieldByName("Hidden").value = false;
+}
